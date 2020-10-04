cedd61521d1d32dcc77dddf63cb0d5f5da2645d3
KostakovaDaria <71877445+KostakovaDaria@users.noreply.github.com>
2020-10-04 15:51:04 +0300

    Update Qube.java

--- lesson3/clean_code/Qube.java
+++ lesson3/clean_code/Qube.java
@@ -1,17 +1,62 @@
 package com.bad.code2;
 
-public class Qube {
+public class Qube implements Shape3D {
 
-	Square square;
+	private Double x;
+	private Double y;
+	private Double z;
+	private Double edgeSize;
+
+	public Qube(Double centerX, Double centerY, Double centerZ, Double s) {
+		
+		this.x = centerX;
+		this.y = centerY;
+		this.z = centerZ;
+		this.edgeSize = s;
+	}
+
+	/**
+	 * Возвращает центральную точку фигуры по оси Х
+	 * 
+	 * @return центральная точка фигуры по оси Х
+	 */
 	
-    public Qube(Double s) {
-        
-    	//в конструкторе определяем базовые фигуры
-    	square = new Square(s);
-    }
+	@Override
+	public Double getX() {
+		return x;
+	}
 
-    //расчет объема на основании базовой фигуры
-    public Double getVolume() {
-        return Math.pow(square.getEdge(), 3);
-    }
+	/**
+	 * Возвращает центральную точку фигуры по оси Y
+	 * 
+	 * @return центральная точка фигуры по оси Y
+	 */
+	
+	@Override
+	public Double getY() {
+		return y;
+	}
+
+	/**
+	 * Возвращает центральную точку фигуры по оси Z
+	 * 
+	 * @return центральная точка фигуры по оси Z
+	 */
+	
+	@Override
+	public Double getZ() {
+		return z;
+	}
+
+	/**
+	 * Возвращает значение объема фигуры
+	 * 
+	 * @return значение объема фигуры
+	 */
+	
+	@Override
+	public Double getVolume() {
+		return Math.pow(edgeSize, 3);
+	}
+
 }
