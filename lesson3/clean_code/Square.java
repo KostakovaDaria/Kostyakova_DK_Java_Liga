06acee685113816794460018b10a06ac40d8764f
KostakovaDaria <71877445+KostakovaDaria@users.noreply.github.com>
2020-10-04 15:49:32 +0300

    Update Square.java

--- lesson3/clean_code/Square.java
+++ lesson3/clean_code/Square.java
@@ -1,27 +1,58 @@
 package com.bad.code2;
 
-public class Square implements Shape{
+public class Square implements Shape2D {
 
-    private Double edgeSize; 
-    
-    public Square(Double edgeSize) {
-       
-    	this.edgeSize = edgeSize;
-    }
-    
-    public Double getEdge()
-    {
-    	return edgeSize;
-    }
-    
-    @Override
-    public Double getArea() {
-        return edgeSize * edgeSize; 
-    }
+	private Double x;
+	private Double y;
+	private Double edgeSize;
 
-	@Override
-	public Double getPerimeter() {
-		return 4 * edgeSize;
+	public Square(Double x, Double y, Double edgeSize) {
+
+		this.x = x;
+		this.y = y;
+		this.edgeSize = edgeSize;
 	}
 
+	/**
+	 * Возвращает центральную точку фигуры по оси Х
+	 * 
+	 * @return центральная точка по оси Х
+	 */
+
+	@Override
+	public Double getX() {
+		return x;
+	}
+
+	/**
+	 * Возвращает центральную точку фигуры по оси Y
+	 * 
+	 * @return центральная точка по оси Y
+	 */
+	
+	@Override
+	public Double getY() {
+		return y;
+	}
+
+	/**
+	 * Возвращает значение стороны фигуры
+	 * 
+	 * @return значение стороны фигуры
+	 */
+	
+	public Double getEdge() {
+		return edgeSize;
+	}
+
+	/**
+	 * Возвращает площадь фигуры
+	 * 
+	 * @return площадь фигуры
+	 */
+	
+	@Override
+	public Double getArea() {
+		return edgeSize * edgeSize;
+	}
 }
