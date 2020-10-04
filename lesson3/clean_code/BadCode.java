207784f1c58cbdbc8bb4eb73dbbe2e07641e56b9
KostakovaDaria <71877445+KostakovaDaria@users.noreply.github.com>
2020-10-04 12:03:33 +0300

    Create BadCode.java

---
+++ lesson3/clean_code/BadCode.java
@@ -0,0 +1,16 @@
+package com.bad.code2;
+
+public class BadCode2 {
+
+	  public static void main(String[] args) {
+	        
+	    	Qube qube = new Qube(10d);
+	        
+	    	System.out.println("Qube volume: " + qube.getVolume());
+
+	        Square square = new Square(5d);
+	        
+	        System.out.println("Square perimeter: " + square.getArea());
+	    }
+	
+}
